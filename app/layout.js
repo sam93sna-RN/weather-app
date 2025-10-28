1cca49f6433d1c603dd4144468b1524989de8a2e
sam93sna-RN <sam93sna@gmail.com>
2025-10-28 16:37:12 +0900

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "福岡 天気予報 ",
+  title: "福岡天気予報 ",
   description: "Next.jsで作成した天気予報アプリ",
 };
 
